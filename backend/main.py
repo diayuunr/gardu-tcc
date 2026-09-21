64bf83f48a05860c64ce6275b8d62a00e6a4091b
diayuunr <diayunuraini9@gmail.com>
2026-09-21 11:54:20 +0700

    fix cors for vercel

--- backend/main.py
+++ backend/main.py
@@ -23,6 +23,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
+        "https://gardu-tcc-ten.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
